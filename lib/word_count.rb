d18d93ebeb14d9836be6614320e0fa8e43080531
Julia Dickey <julia.dickey@gmail.com>
2016-07-01 08:50:04 -0700

    Add code to rb file to fulfill the first spec. One example, zero failures.

---
+++ lib/word_count.rb
@@ -0,0 +1,17 @@
+require('pry')
+
+class String
+  define_method(:count) do |phrase|
+
+    select_word = self.downcase()
+    phrase_array = phrase.downcase().split(" ")
+    counter = 0
+
+    phrase_array.each do |phrase_word|
+      if select_word == phrase_word
+        counter = counter.+(1)
+      end
+    end
+    counter.to_s()
+  end
+end
