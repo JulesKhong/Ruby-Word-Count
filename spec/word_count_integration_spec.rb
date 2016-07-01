753e71ccf93d65f33f63a8cc19ce834dd604cc7e
Julia Dickey <julia.dickey@gmail.com>
2016-07-01 11:02:45 -0700

    Update files as I switch to a desktop. No progress on input disappearance

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -6,9 +6,9 @@ set(:show_exceptions, false)
 describe('the search results',{:type => :feature}) do
   it 'takes a word and phrase, and returns the frequency of the word in the phrase' do
     visit('/')
-    fill_in('input',:with => "sea")
-    fill_in('phrase',:with => "She sells sea shells by the sea shore")
-    click_button('Submit')
+    fill_in 'input', :with => "cat"
+    fill_in 'phrase', :with => "She sells sea shell cat by the sea cat shore"
+    click_button 'Submit'
     expect(page).to have_content('2')
   end
 end
