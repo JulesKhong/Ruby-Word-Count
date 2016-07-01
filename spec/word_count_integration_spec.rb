8c8c7356741f79332c91cd0826688caace303ab8
Julia Dickey <julia.dickey@gmail.com>
2016-07-01 16:51:35 -0700

    Finish for the day. Bug remains, and the page will not currently load.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -3,12 +3,18 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the search for a word', {:type => :feature}) do
+describe('the search for a word',{:type => :feature}) do
+  it ('should first load the index') do
+    expect(page).to have_content("Check your frequency")
+  end
+end
+
+describe('the search for a word',{:type => :feature}) do
   it ('takes a word and phrase and returns the frequency of the word in the phrase') do
     visit('/')
     fill_in('input', :with => 'sea')
     fill_in('list', :with => 'She sells sea shells by the sea shore')
     click_button('Submit')
-    expect(page).to have_content('4')
+    expect(page).to have_content('2')
   end
 end
